8517efb2418054f575db48eaa4b67c74454d42b1
pyro161 <jsbhullar16@yahoo.com>
2024-09-29 10:46:01 -0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Header from '$lib/components/layout/Header.svelte'
+	import '$lib/global.css';
+	const { children } = $props();
+</script>
+
+<main class="bg-base-200 overflow-auto flex flex-col min-h-screen">
+	<Header />
+	{@render children()}
+</main>
